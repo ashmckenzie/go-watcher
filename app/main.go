f2178b6c72ed2cfd0f77c3ae05c933e37a05eab1
Ash McKenzie <ash@the-rebellion.net>
2017-02-04 04:05:10 +1100

    Seems to work :)

--- app/main.go
+++ app/main.go
@@ -4,67 +4,95 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"sync"
+	"time"
 
-	"github.com/davecgh/go-spew/spew"
-	pushbullet "github.com/mitsuse/pushbullet-go"
-	"github.com/mitsuse/pushbullet-go/requests"
+	"github.com/nats-io/go-nats-streaming"
 	"github.com/rjeczalik/notify"
 	"github.com/urfave/cli"
 )
 
-func sendNote(apiKey string, title string, message string) {
-	pb := pushbullet.New(apiKey)
+func publish(subject string, path string) {
+	clusterID := "test-cluster"
+	clientID := "go-watcher-new-movies"
+	URL := stan.DefaultNatsURL
 
-	n := requests.NewNote()
-	n.Title = title
-	n.Body = message
+	sc, err := stan.Connect(clusterID, clientID, stan.NatsURL(URL))
+	if err != nil {
+		log.Fatalf("Can't connect: %v.\nMake sure a NATS Streaming Server is running at: %s", err, URL)
+	}
+	defer sc.Close()
 
-	if _, err := pb.PostPushesNote(n); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		return
+	ch := make(chan bool)
+	var glock sync.Mutex
+	var guid string
+
+	acb := func(lguid string, err error) {
+		glock.Lock()
+		// log.Printf("Received ACK for guid %s\n", lguid)
+
+		defer glock.Unlock()
+		if err != nil {
+			log.Fatalf("Error in server ack for guid %s: %v\n", lguid, err)
+		}
+		if lguid != guid {
+			log.Fatalf("Expected a matching guid in ack callback, got %s vs %s\n", lguid, guid)
+		}
+		ch <- true
+	}
+
+	glock.Lock()
+	guid, err = sc.PublishAsync(subject, []byte(path), acb)
+	if err != nil {
+		log.Fatalf("Error during async publish: %v\n", err)
+	}
+	glock.Unlock()
+	if guid == "" {
+		log.Fatal("Expected non-empty guid to be returned.")
+	}
+	// log.Printf("Published [%s] : '%s' [guid: %s]\n", subject, path, guid)
+
+	select {
+	case <-ch:
+		break
+	case <-time.After(5 * time.Second):
+		log.Fatal("timeout")
 	}
 }
 
 func main() {
-	var title string
-
 	app := cli.NewApp()
 	app.Name = "watcher"
 	app.Usage = "Watch a bunch of directories and notify of change"
 	app.Version = "0.1.0"
 
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "title, t",
-			Usage:       "note title",
-			EnvVar:      "TITLE",
-			Destination: &title,
-		},
-	}
-
 	app.Action = func(c *cli.Context) error {
-		apiKey := os.Getenv("PUSHBULLET_API_KEY")
-
-		if len(apiKey) == 0 {
-			return cli.NewExitError("ERROR: API key is empty!", 1)
+		if len(c.Args()) != 1 {
+			log.Fatal("Please specify a directory to watch")
 		}
 
-		spew.Dump(c.Args())
+		path := strings.TrimSpace(strings.TrimRight(c.Args()[0], "/"))
+		fullPath := fmt.Sprintf("%s/...", path)
 
 		eventChannel := make(chan notify.EventInfo, 1)
 
-		if err := notify.Watch("/tmp/...", eventChannel, notify.Create); err != nil {
+		if err := notify.Watch(fullPath, eventChannel, notify.Create); err != nil {
 			log.Fatal(err)
 		}
 
 		defer notify.Stop(eventChannel)
 
+		log.Printf("Watching %s", fullPath)
+
 		for {
 			ei := <-eventChannel
-			log.Println("Got event:", ei)
+			path := ei.Path()
+			if info, err := os.Stat(path); err == nil && !info.IsDir() {
+				log.Println("New movie:", path)
+				publish("new-movie", path)
+			}
 		}
-
-		// sendNote(apiKey, title, message)
 	}
 
 	app.Run(os.Args)
